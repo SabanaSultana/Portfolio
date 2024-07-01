cb3193daaf33d389fb3479ec6939db85f86f6215
Sabana <sabanasultana132@gmail.com>
2024-07-01 12:14:45 +0530

    "update css of home "

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,14 +6,14 @@ const Home = () => {
   return (
     <div className="home">
       <div className="leftbar">
-        <p className="greet">Hii 👋, I am </p>
+        <p className="greet">Hello, I am</p>
         <p className="name">Sabana Sultana</p>
         <div className="typewriter-container">
           <p>And I am a</p>
           <div className="typewriter-text">
             <Typewriter
               options={{
-                strings: ["MERN Stack Developer!", "Data Science Enthusiast."],
+                strings: ["MERN STACK DEVELOPER", "ML ENTHUSIAST"],
                 autoStart: true,
                 loop: true,
               }}
@@ -21,7 +21,15 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="rightbar"></div>
+      <div className="rightbar">
+        <div className="avatar-container">
+          <img
+            src="https://img.freepik.com/premium-photo/teenager-girl-wearing-headphones-studying-laptop-room-blue-neon-lights-lofi-background_938529-705.jpg"
+            alt="Avatar"
+            className="avatar"
+          />
+        </div>
+      </div>
     </div>
   );
 };
