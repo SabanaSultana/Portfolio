cc5e8db5469bb4d4b45ef8f52191f8cbed72069c
Sabana <sabanasultana132@gmail.com>
2024-07-03 18:09:55 +0530

    added dist file on server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const mongoose=require("mongoose")
-const MessageModel=require("./models/portfolioModel")
+const mongoose = require("mongoose");
+const MessageModel = require("./models/portfolioModel");
+const path = require("path");
 
 dotenv.config();
 
@@ -11,22 +12,22 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
-
-//routes
-// app.get('/',(req,res)=>{
-//     res.send(`<h1>Welcome to  Node Server</h1>`)
-// })
+//static files
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 mongoose.connect("mongodb://127.0.0.1:27017/Portfolio");
 // app.use("/api/v1/portfolio", require("./routes/portfolioRoutes"));
 
 const port = process.env.PORT || 8080;
-app.post('/sendMessage',(req,res)=>{
-      MessageModel.create(req.body)
-      .then(messages=> res.json(messages))
-      .catch(err=>res.json(err))
-})
+app.post("/sendMessage", (req, res) => {
+  MessageModel.create(req.body)
+    .then((messages) => res.json(messages))
+    .catch((err) => res.json(err));
+});
 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 //listen
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
